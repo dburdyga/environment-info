2105e8f55d192c102226a303a104ac66fb172dd0
Daria <dariaburdyga@gmail.com>
2019-09-12 14:14:21 +0300

    removed Footer and Header

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,16 @@
 <template>
   <div id="app">
-<!--    <Header v-if="$route.name !== 'vue-telekom'"/>-->
     <div class="view">
       <router-view/>
     </div>
-<!--    <Footer v-if="$route.name !== 'vue-telekom'"/>-->
   </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue';
-  import Header from './components/root/Header/Header.vue';
-  import Footer from './components/root/Footer/Footer.vue';
 
   export default Vue.extend({
-    name: 'App',
-    components: {
-      Header,
-      Footer
-    }
+    name: 'App'
   });
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <Posts msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Environment msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import Posts from '@/components/Environment.vue'; // @ is an alias to /src
+import Environment from '@/components/Environment.vue'; // @ is an alias to /src
 
 export default Vue.extend({
   name: 'home',
   components: {
-    Posts
+    Environment
   }
 });
 </script>
